da7d471931bf664d5421c69384429c8e37862175
Jonatan Alexis Anauati <barakwins@gmail.com>
2012-05-21 15:08:05 -0300

    Adds the `get_current_site` method.
    By examining the current request, this method returns the related Site object
    (`django.contrib.sites.models.Site`).

--- jaa/django/globalrequest/context.py
+++ jaa/django/globalrequest/context.py
@@ -1,19 +1,46 @@
+# -*- coding: utf-8 -*-
+
 import threading
+from django.conf import settings
+
+try:
+    from django.contrib import sites
+except:
+    sites = None
+
 
 if not 'context' in locals():
     context = threading.local()
 
+def _get_context():
+    return context
+
 def get_request():
-    return getattr(context, 'request', None)
+    return getattr(_get_context(), 'request', None)
 
 def set_request(request):
-    context.request=request
+    _get_context().request=request
 
 def get_current_request():
     return get_request()
 
+def set_current_user(user):
+    _get_context().user = user
+
 def get_current_user():
     try:
-        return get_request().user
+        return getattr(_get_context(),'user', get_request().user)
     except:
         return None
+
+def get_current_site():
+    try:
+        asd
+        return sites.models.get_current_site(get_current_request())
+    except:
+        pass
+    try:
+        return sites.models.Site.objects.get(pk=settings.SITE_ID)
+    except:
+        pass
+    return None
